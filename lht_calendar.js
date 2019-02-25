975c2bf62958ab53f33c890e73d948229d41d645
Connor Webdale <CWebdale006@west-mec.org>
2019-02-25 15:23:34 -0700

    Created a function
    Created a function to find the days in each month, and correct itself for a leap year.

--- lht_calendar.js
+++ lht_calendar.js
@@ -40,6 +40,7 @@ document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 function createCalendar(calDate) {
       var calendarHTML = "<table id='calendar_table'>";
       calendarHTML += calCaption(calDate);
+      calendarHTML += calWeekdayRow();
       calendarHTML += "</table>";
       return calendarHTML;
 }
@@ -58,3 +59,61 @@ function calCaption(calDate) {
       // Writes the caption 
       return "<caption>" + monthName[thisMonth] + " " + thisYear + "</caption>";
 }
+
+// function to write a table row of weekday abbreviations 
+function calWeekdayRow() {
+      // array of weeksday abbreviations 
+      var dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+      var rowHTML = "<tr>"
+
+      // Looks through the dayName array 
+      for (var i = 0; i < dayName.length; i++) {
+            rowHTML += "<th class='calendar_weekdays'>" + dayName[i] + "</th>";
+      }
+      rowHTML += "</tr>";
+      return rowHTML;
+}
+
+// function to calculate the number of days in the month 
+
+function daysInMonth(calDate) {
+      //array of days in each month 
+      var dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+      //extract the four digit year and month value 
+      var thisYear = calDate.getFullYear();
+      var thisMonth = calDate.getMonth();
+
+      // revise the days in February for leap years 
+      if (thisYear % 4 === 0) {
+            dayCount[1] = 29;
+      }
+
+      // return the number of days for the current month 
+      return dayCount[thisMonth];
+}
+
+
+
+
+
+
+// my array clawson told me to make 
+
+// var grade = [90, 89, 95, 78, 84, 99, 67, 55, 89];
+
+// for (let i = 0; i < grade.length; i++) {
+//       grade.forEach(alphaGrade());
+
+//       function alphaGrade(value) {
+//             if (value >= 90) {
+//                   alphaGrade = "A";
+//             } else {
+//                   if (value >= 80 {
+//                         alphaGrade = "B"; 
+//                         } else {
+
+//                         }
+//                   }
+//             }
+//       }
