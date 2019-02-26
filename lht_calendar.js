3263523b427ba0589799f9b0351b285e51053f45
Connor Webdale <CWebdale006@west-mec.org>
2019-02-26 15:12:18 -0700

    Created a function to write a table
    Created a function to write table rows for each day of the month, using for loops and if/else statements.

--- lht_calendar.js
+++ lht_calendar.js
@@ -30,8 +30,8 @@
 	
 */
 
-// Sets the date displayed in the vcalendar. 
-var thisDay = new Date("August 24, 2018");
+// Sets the date displayed in the calendar. 
+var thisDay = new Date();
 
 // Write the calendar to the element with the id "calendar"
 document.getElementById("calendar").innerHTML = createCalendar(thisDay);
@@ -41,6 +41,7 @@ function createCalendar(calDate) {
       var calendarHTML = "<table id='calendar_table'>";
       calendarHTML += calCaption(calDate);
       calendarHTML += calWeekdayRow();
+      calendarHTML += calDays(calDate);
       calendarHTML += "</table>";
       return calendarHTML;
 }
@@ -86,34 +87,80 @@ function daysInMonth(calDate) {
 
       // revise the days in February for leap years 
       if (thisYear % 4 === 0) {
-            dayCount[1] = 29;
+            if ((thisYear % 100 != 0) || (thisYear % 400 === 0)) {
+                  dayCount[1] = 29;
+            }
       }
 
       // return the number of days for the current month 
       return dayCount[thisMonth];
 }
 
+// Function to write the table rows for each day of the month 
+function calDays(calDate) {
+      // determine the starting day of the month 
+      var day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+      var weekDay = day.getDay();
+
+      // write blank cells preceding the starting day 
+      var htmlCode = "<tr>";
+      for (var i = 0; i < weekDay; i++) {
+            htmlCode += "<td></td>";
+      }
+
+      // write cells for each day of the month 
+      var totalDays = daysInMonth(calDate);
+
+      var highlightDay = calDate.getDate();
+
+      for (var i = 1; i <= totalDays; i++) {
+            day.setDate(i);
+            weekDay = day.getDay();
+
+            if (weekDay === 0) {
+                  htmlCode += "<tr>";
+            }
+
+            if (i === highlightDay) {
+                  htmlCode += "<td class='calendar_dates' id='calendar_today'>" + i + dayEvent[i] + "</td>";
+            } else {
+                  htmlCode += "<td class='calendar_dates'>" + i + dayEvent[i] + "</td>";
+            }
+
+            if (weekDay === 6) {
+                  htmlCode += "</tr>";
+            }
+      }
+      return htmlCode;
+}
 
 
 
 
-
-// my array clawson told me to make 
+// my (broken) array clawson told me to make 
 
 // var grade = [90, 89, 95, 78, 84, 99, 67, 55, 89];
 
-// for (let i = 0; i < grade.length; i++) {
-//       grade.forEach(alphaGrade());
+
+// grade.forEach(alphaGrade(value)) {
 
 //       function alphaGrade(value) {
 //             if (value >= 90) {
 //                   alphaGrade = "A";
 //             } else {
-//                   if (value >= 80 {
-//                         alphaGrade = "B"; 
+//                   if (value >= 80) {
+//                         alphaGrade = "B";
+//                   } else {
+//                         if (value >= 70) {
+//                               alphaGrade = "C";
 //                         } else {
-
+//                               if (value >= 60) {
+//                                     alphaGrade = "D";
+//                               } else {
+//                                     alphaGrade = "F";
+//                               }
 //                         }
 //                   }
 //             }
 //       }
+// }
